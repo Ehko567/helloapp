65aa6673ded4f2e1839ed2dcfccb75b625154dfb
black <black@example.com>
2023-11-15 16:39:24 +0500

    all fixed

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-
 def main(name):
     # Use a breakpoint in the code line below to debug your script.
     print('hello everyone')  # Press Ctrl+F8 to toggle the breakpoint.
 
+def greetBlack():
+    print('hello black')
+
+def greetWhite():
+    print('hello white')
+
+main('1')
+greetWhite()
