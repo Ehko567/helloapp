f423ac1d172a4d8fdb9e60f4bd52d3e83db1db7e
black <black@example.com>
2023-11-15 16:31:44 +0500

    add greeting black

--- main.py
+++ main.py
@@ -3,3 +3,5 @@ def main(name):
     # Use a breakpoint in the code line below to debug your script.
     print('hello everyone')  # Press Ctrl+F8 to toggle the breakpoint.
 
+def greetBlack():
+    print('hello black')
